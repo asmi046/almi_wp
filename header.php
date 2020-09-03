5246cd04817a44ea66ea33cc7398b374e25bc6b8
asmi046 <asmi046@gmail.com>
2020-09-03 14:22:15 +0300

    Добавил Фав иконки и модальные окна

--- header.php
+++ header.php
@@ -3,36 +3,27 @@
 <head profile="http://gmpg.org/xfn/11"> 
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <title><?php wp_title(); ?></title>
-    
-    <meta name="viewport" content="width=device-width">
-	
-	<script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
-	
-    <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/icon.png" />
+    	
     <?php wp_head();?> 
 	
 </head> 
 <body>
-<!-- Арктик модал -->
-<div style="display: none;">
-    <div class="box-modal" id="messgeModal">
-        <div class="box-modal_close arcticmodal-close">закрыть</div>
-        <div class = "modalline" id = "lineIcon">
-		</div>
-		
-		<div class = "modalline" id = "lineMsg">
-		</div>
-    </div>
-</div>
 
-	<script>
-		$(document).ready(function() { 
-	
-		});		
-	</script>
-
-<div class = "main">
+<!-- Скрипт для вывода яндекс карт Подключать непосредственно перед вызовом скрипта инициализации карты-->
+<!-- <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script> -->
 
 <!-- Меню -->
-<?php $options = get_option( "wpuniq_theme_options" ); ?>
 <?php wp_nav_menu( array('menu' => 'Главное меню', 'container' => false )); ?>
+
+<!-- Подключение  модальных окон-->
+<? include "modal-win.php";?>
+
+<!-- Блок вывода иконок -->
+<!-- 
+<link rel="icon" type="image/png" sizes="256x256" href="<?php echo get_template_directory_uri();?>/img/favicons/icon256.png">
+  <link rel="icon" type="image/png" sizes="128x128" href="<?php echo get_template_directory_uri();?>/img/favicons/icon128.png">
+  <link rel="icon" type="image/png" sizes="64x64" href="<?php echo get_template_directory_uri();?>/img/favicons/icon64.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/img/favicons/icon32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/img/favicons/icon16.png">
+  <link rel="icon" type="image/svg+xml" sizes="any" href="/images/icons/any-109fcff231.svg"> 
+-->
