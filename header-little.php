37e81d7c71976e76cc2035d3d54d278180cd82c2
pavlik1l <pavlik1l@ya.ru>
2020-09-04 17:58:26 +0300

    little-page

---
+++ header-little.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="zxx">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="minimum-scale=1.0, target-densitydpi=device-dpi, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="MobileOptimized" content="320">
+    <title>Almi</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="wrapper" class="little-almi-page">
+        <header id="header">
+            <div class="large-decor-elem large-decor-elem__kids"> 
+            </div>
+            <div class="inner">
+                <a href="#" class="burger-btn">
+                    <div class="burger-btn__inner">
+                        <span></span>
+                    </div>
+                </a>
+                <div class="nav-panel clearfix">
+                    <button class="close-btn abs close-menu-btn"></button>
+                    <a href="#" class="home-link inb vT"></a>
+                    <nav class="main-menu inb vT">
+                        <ul>
+                            <li><a href="#">Little ALMI</a></li>
+                            <li><a href="#">PET'ALMI</a></li>
+                            <li><a href="#">Payment and delivery</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Reviews</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </nav>
+                    <div class="contacts-elem fright">
+                        <a href="email:info@almi.ru" class="contacts-elem__email inb vT">info@almi.ru</a>
+
+                        <div class="phone-box inb vT rL desktop">
+                            <span>8 800 555 44 41</span>
+                        </div>
+
+                        <a href="tel:88005554441" class="phone-box inb vT rL mobil">
+                            <span>8 800 555 44 41</span>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="great-shadow"></div>
+                <div class="logo__box logo__box-large">
+                    <div class="logo__item">
+                        <img src="image/littleALMI.svg" class="spacer" alt="Pet`almi logo">
+                    </div>
+                </div>
+                <div class="header-content">
+                    <h2>Goods for kids</h2>
+                    <h1>Little ALMI</h1>
+                    <p>
+                        Made of high quality nylon fabric to ensure safety, features the solid zinc alloy swivel snap and metal buckles guarantee extra durability
+                    </p>
+                    <p>
+                        Note: the tab on the seatbelt clip is 2cm, please check your buckle's size and compatibility before purchase
+                    </p> 
+                    <a href="#" class="btn">To get a consultation</a>
+                </div>
+            </div>
+        </header>
