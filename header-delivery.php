376e6b7276aca068958b0c3561ea04d6a1e0b431
pavlik1l <pavlik1l@ya.ru>
2020-09-06 15:43:04 +0300

    Доставка

---
+++ header-delivery.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="zxx">
+
+<head profile="http://gmpg.org/xfn/11"> 
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <title><?php wp_title(); ?></title>
+    	
+    <?php wp_head();?> 
+	
+</head> 
+
+<body>
+    <div id="wrapper" class="pd-page">
+        <header id="header">
+            <div class="middle-decor-elem middle-decor-elem__pd"> 
+            </div>
+            <div class="inner">
+                <a href="#" class="burger-btn">
+                    <div class="burger-btn__inner">
+                        <span></span>
+                    </div>
+                </a>
+                <div class="nav-panel clearfix">
+                    <button class="close-btn abs close-menu-btn"></button>
+                    <a href="#" class="home-link inb vT"></a>
+                    <nav class="main-menu inb vT">
+                        <ul>
+                            <li><a href="#">Little ALMI</a></li>
+                            <li><a href="#">PET'ALMI</a></li>
+                            <li><a href="#">Payment and delivery</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Reviews</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </nav>
+                    <div class="contacts-elem fright">
+                        <a href="email:info@almi.ru" class="contacts-elem__email inb vT">info@almi.ru</a>
+
+                        <div class="phone-box inb vT rL desktop">
+                            <span>8 800 555 44 41</span>
+                        </div>
+
+                        <a href="tel:88005554441" class="phone-box inb vT rL mobil">
+                            <span>8 800 555 44 41</span>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="great-shadow"></div>
+                <div class="logo__box logo__box-small">
+                   <div class="logo__item">
+                        <img src="image/littleALMI.svg" class="spacer" alt="Pet`almi logo">
+                    </div>
+                    <div class="logo__item">
+                        <img src="image/pet-almi.svg" class="spacer" alt="Pet`almi logo">
+                    </div>
+                </div>
+                <div class="header-content"> 
+                    <h1>Payment and delivery</h1>  
+                </div>
+            </div>
+        </header>
