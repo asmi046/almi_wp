00ac602d0bfd844970a9aff77cdcb68d972253e0
pavlik1l <pavlik1l@ya.ru>
2020-09-08 02:28:37 +0300

    Рейтинг

--- template-parts/product-loop.php
+++ template-parts/product-loop.php
@@ -10,11 +10,24 @@
             <?php the_title();?>
         </p>
         <ul class="star-track">
-            <li class="star-full star-track__item"></li>
+            <?php 
+            $inc = 1;
+            foreach($rating = carbon_get_the_post_meta('rating') as $star):?>
+
+                <?php 
+                if($inc < 6):
+                    if($rating >= $inc):?>
+                        <li class="star-full star-track__item"></li>
+                    <?php else:?>
+                        <li class="star-empty star-track__item"></li>
+                    <?php endif;
+                endif;?>
+            <?php endforeach;?>
+            <!-- <li class="star-full star-track__item"></li>
             <li class="star-full star-track__item"></li>
             <li class="star-full star-track__item"></li>
             <li class="star-half star-track__item"></li>
-            <li class="star-empty star-track__item"></li>
+            <li class="star-empty star-track__item"></li> -->
         </ul>
         <span class="catalog__item__price db"><?php echo carbon_get_the_post_meta('price');?></span>
         <div class="catalog__link-box">
