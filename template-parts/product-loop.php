a31c72ca5751f1c5c20beba35fe2b6a9e3aed6ff
pavlik1l <pavlik1l@ya.ru>
2020-09-08 02:20:38 +0300

    product-loop

--- template-parts/product-loop.php
+++ template-parts/product-loop.php
@@ -4,10 +4,10 @@
             2 Packs
         </div>
         <div class="catalog__item__img cover" style="background-image: url(image/products-for-pets-4.jpg)">
-            <img src="img/catalog__item__img-spacer.png" alt="" class="spacer">
+            <img src="<?php echo get_the_post_thumbnail_url();?>" alt="" class="spacer">
         </div>
         <p>
-            URPOWER Upgraded Dog Seat Belt 2 Pack Car Seat Belt for Dogs, Cats and Pets
+            <?php the_title();?>
         </p>
         <ul class="star-track">
             <li class="star-full star-track__item"></li>
@@ -16,10 +16,10 @@
             <li class="star-half star-track__item"></li>
             <li class="star-empty star-track__item"></li>
         </ul>
-        <span class="catalog__item__price db">$10.99</span>
+        <span class="catalog__item__price db"><?php echo carbon_get_the_post_meta('price');?></span>
         <div class="catalog__link-box">
             Buy on
-            <a href="#" class="amazon-link"></a>
+            <a href="<?php echo carbon_get_the_post_meta('amazon_link');?>" class="amazon-link"></a>
         </div>
     </div>
 
