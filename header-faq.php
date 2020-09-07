5d266ffe6314f55532c4ae2eebfb56f15a868808
pavlik1l <pavlik1l@ya.ru>
2020-09-07 17:37:03 +0300

    Header

--- header-faq.php
+++ header-faq.php
@@ -19,10 +19,10 @@
                 <?php get_template_part('template-parts/top-header');?>
                 <div class="logo__box logo__box-large">
                    <div class="logo__item">
-                        <img src="image/littleALMI.svg" class="spacer" alt="Pet`almi logo">
+                        <img src="<?php echo get_template_directory_uri();?>/image/littleALMI.svg" class="spacer" alt="Pet`almi logo">
                     </div>
                     <div class="logo__item">
-                        <img src="image/pet-almi.svg" class="spacer" alt="Pet`almi logo">
+                        <img src="<?php echo get_template_directory_uri();?>/image/pet-almi.svg" class="spacer" alt="Pet`almi logo">
                     </div>
                 </div>
                 <div class="header-content"> 
--- header.php
+++ header.php
@@ -36,7 +36,7 @@
                 <?php get_template_part('template-parts/top-header');?>
                 <div class="logo__box logo__box-large">
                     <div class="logo__item">
-                        <img src="image/pet-almi.svg" class="spacer" alt="Pet`almi logo">
+                        <img src="<?php echo get_template_directory_uri();?>/image/pet-almi.svg" class="spacer" alt="Pet`almi logo">
                     </div>
                 </div>
                 <div class="header-content">
