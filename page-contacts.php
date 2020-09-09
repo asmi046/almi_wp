551be7f8d53a8be3c01125aad7bd062377c6f520
pavlik1l <pavlik1l@ya.ru>
2020-09-09 14:56:06 +0300

    Update page-contacts.php

--- page-contacts.php
+++ page-contacts.php
@@ -7,6 +7,39 @@ get_header('contacts');?>
        <main class="main">
             <div class="inner">
                 <div id="map"></div>
+                <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+                <script>
+                    ymaps.ready(init);
+
+                    function init () {
+                    
+                        var myMap = new ymaps.Map("mapLine", {
+                                center: <?php echo carbon_get_theme_option('mkad_map_point') ?>,
+                                zoom: 14,
+                                controls: ['zoomControl']
+                            }),
+
+                        myPlacemarkAdr = new ymaps.Placemark(<?php echo carbon_get_theme_option('mkad_map_point') ?>, {
+                                iconContent: '',
+                                balloonContent: 'Наш адрес: <b><?php echo carbon_get_theme_option('address') ?></b><br/>Телефон: <b> <?php echo carbon_get_theme_option('tel') ?>',
+                                hintContent: 'Наш адрес: <b><?php echo carbon_get_theme_option('address') ?></b><br/>Телефон: <b> <?php echo carbon_get_theme_option('tel') ?>',
+                            }, {
+                            iconLayout: 'default#image',
+                            iconImageHref: '<?php bloginfo("template_url"); ?>/img/pointermapmaster.svg',
+                            iconImageSize: [30, 54],
+                            iconImageOffset: [-15, -54]
+                            });
+                            
+                            myMap.geoObjects.add(myPlacemarkAdr);
+                            
+                        
+                        
+                        
+
+
+                    myMap.behaviors.disable('scrollZoom');
+                    }
+                </script>
                 <form class="feedback clearfix rL">
                     <div class="form-contaner">
                         <h3 class="form-name">Feedback </h3>
