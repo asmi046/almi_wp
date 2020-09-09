af24371acf7b47eb3a7f183c8f405842b2025927
pavlik1l <pavlik1l@ya.ru>
2020-09-09 14:51:54 +0300

    Update themes-fields.php

--- themes-fields.php
+++ themes-fields.php
@@ -17,8 +17,11 @@ Container::make( 'theme_options', __( 'Настройки темы', 'crb' ) )
 				->set_width(30),
 			Field::make('text', 'tel', 'Телефон')
 				->set_width(30),
-				Field::make('text', 'address', 'Адрес')
-					->set_width(30),
+			Field::make('text', 'address', 'Адрес')
+				->set_width(30),
+			Field::make('text', 'mkad_map_point', 'Центр карты')
+				->set_width(30),
+					
 			Field::make( 'text', 'crb_text', 'Text Field' ),
 		) );
 		
