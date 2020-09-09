9edfc35556572ab7a321f6902fb30be9d48f32c6
pavlik1l <pavlik1l@ya.ru>
2020-09-09 22:20:58 +0300

    форма заявки

--- page-contacts.php
+++ page-contacts.php
@@ -40,22 +40,7 @@ get_header('contacts');?>
                     myMap.behaviors.disable('scrollZoom');
                     }
                 </script>
-                <form class="feedback clearfix rL">
-                    <div class="form-contaner">
-                        <h3 class="form-name">Feedback </h3>
-                        <span class="form-text">
-                            Leave a request and our specialists will answer all your questions
-                        </span>
-                        <div class="field-box rL">
-                            <input type="text" class="inputbox" placeholder="Name">
-                            <input type="tel" class="inputbox" placeholder="Telephone">
-                            <input type="submit" class="btn" value="Send message">
-                        </div>
-                    </div>
-                    <div class="feedback__decor feedback__decor__headhpones">
-                        <img src="<?php echo get_template_directory_uri();?>/img/headphones.png" alt="pets" class="spacer">
-                    </div>
-                </form>
+                <?php get_template_part('template-parts/feedback');?>
             </div>
             <div class="subfooter"></div>
         </main>
