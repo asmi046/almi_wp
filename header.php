6421d5b848fc8cbb7264898b1f1d83d979fe9a31
asmi046 <asmi046@gmail.com>
2020-09-15 08:41:22 +0300

    Добавил иконки

--- header.php
+++ header.php
@@ -7,6 +7,13 @@
     <meta name="HandheldFriendly" content="true">
     <meta name="MobileOptimized" content="320">
     
+    <link rel="icon" type="image/png" sizes="256x256" href="<?php echo get_template_directory_uri();?>/img/favicons/icon256.png">
+    <link rel="icon" type="image/png" sizes="128x128" href="<?php echo get_template_directory_uri();?>/img/favicons/icon128.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="<?php echo get_template_directory_uri();?>/img/favicons/icon64.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/img/favicons/icon32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/img/favicons/icon16.png">
+    <link rel="icon" type="image/svg+xml" sizes="any" href="<? echo get_template_directory_uri();?>/image/pet-almi.svg">
+
     <title><?php wp_title(); ?></title>
     	
     <?php wp_head();?> 
@@ -25,7 +32,7 @@
 
 <!-- Блок вывода иконок -->
 <!-- 
-<link rel="icon" type="image/png" sizes="256x256" href="<?php echo get_template_directory_uri();?>/img/favicons/icon256.png">
+  <link rel="icon" type="image/png" sizes="256x256" href="<?php echo get_template_directory_uri();?>/img/favicons/icon256.png">
   <link rel="icon" type="image/png" sizes="128x128" href="<?php echo get_template_directory_uri();?>/img/favicons/icon128.png">
   <link rel="icon" type="image/png" sizes="64x64" href="<?php echo get_template_directory_uri();?>/img/favicons/icon64.png">
   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/img/favicons/icon32.png">
