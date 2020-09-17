48def7bb93a06719d9a31c615d53c331f0163905
asmi046 <asmi046@gmail.com>
2020-09-17 15:27:28 +0300

    Переверстанное окно отзыва

--- modal-win.php
+++ modal-win.php
@@ -60,12 +60,13 @@
 		</div>
 
 	<!-- Popup-2 -->
+<!-- 
 	<div id="popupS" class="popup popup-two" style="display: none;">
 
     <div id="popup__bodyS" class="popup__body box-modal">
       <div class="box-modal_close arcticmodal-close">Close</div>
 				<div class="popup__content popup__content-two">
-       		<!-- <a href="#" class="popup__close"></a> -->
+    
 
        		<div class="popup__color"></div>
 
@@ -86,14 +87,15 @@
         	<div class="popup__form_right">
 						<div class="popup__assess">
 							<h3 class="popup__assess_title">Assessement</h3>
-							<div class="popup__stars">
-								<div class="popup__stars_rating"></div>
-								<div class="popup__stars_rating"></div>
-								<div class="popup__stars_rating"></div>
-								<div class="popup__stars_rating"></div>
-								<div class="popup__stars_rating"></div>
-									Product Quality	
-							</div>
+							<ul class="star-track">
+								<li class="star-empty star-track__item"></li>
+								<li class="star-empty star-track__item"></li>
+								<li class="star-empty star-track__item"></li>
+								<li class="star-empty star-track__item"></li>
+								<li class="star-empty star-track__item"></li>
+							</ul>
+							Product Quality	
+
 							<div class="popup__stars">
 								<div class="popup__stars_rating"></div>
 								<div class="popup__stars_rating"></div>
@@ -117,4 +119,67 @@
 
     	</div>
 		</div>
+	</div> -->
+
+	<div id="popup_2" class="popup popup-two">
+
+<div id="popup__bodyS" class="popup__body box-modal">
+  <div class="box-modal_close arcticmodal-close">Close</div>
+		<div class="popup__content popup__content-two">
+		   
+		   <div class="popup__color"></div>
+
+		   <div class="block-card">
+			   <div class="popup__content-card popup__content-card-two">
+				<div class="popup__title">
+					<h3 class="popup__title_text">
+						Give feedback
+					</h3>
+			</div>
+			<form class="popup__form popup__form_two" action="#" method="get" >
+				<input class = "inputbox" type="text" name="name" placeholder="Name">
+				<input  class = "inputbox" type="text" name="tel" placeholder="Telephone">
+				<textarea name = "reviews" placeholder="Review"></textarea>
+			</form>
+		</div>
+		<div class="popup__form_right">
+					<div class="popup__assess">
+
+						<div class="popup-block__assess">
+						<h3 class="popup__assess_title">Assessement</h3>
+						<div class="popup__stars">
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>
+								Product Quality	
+						</div>
+						<div class="popup__stars">
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>
+							<div class="popup__stars_rating"></div>	
+							Brand Quality
+						</div>
+						</div>	
+						<div class="popup-block__form">
+							<form class="popup__input" action="#" method="get">
+								<div id="file-name" class="popup__upload"  placeholder="File.txt">File.txt</div>
+								<label id="mylabel"><input id="myfile" type="file" name="file" onchange="getFileName ();">Upload file</label>
+							</form>								
+							<form class="popup__input" action="#" method="get">
+								<div id="file-names" class="popup__upload"  placeholder="File.txt">File.txt</div>
+								<label id="mylabel"><input id="myfiles" type="file" name="file" onchange="getFileName ();">Upload file</label>
+							</form>
+							</div>
+
+					</div>
+				</div>
+				<button class="popup__btn popup-2__btn btn">Send message</button>
+				</div>
+
 	</div>
+	</div>
+</div>
