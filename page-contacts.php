d7a12cdd32cd15d05ab4baf46ab68ba3edc2281a
asmi046 <asmi046@gmail.com>
2020-09-23 08:08:08 +0300

    Новая карта от GOOGLE

--- page-contacts.php
+++ page-contacts.php
@@ -7,10 +7,12 @@ get_header();
 get_template_part('template-parts/header-contacts');?>
        <main class="main">
             <div class="inner">
-                <div id="map"></div>
+                <div id="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3052.9851079316713!2d-76.30769688461444!3d40.07574427940622!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6236e218d3d7d%3A0xb5c10fff9e5ae24d!2zMzJEIEUgUm9zZXZpbGxlIFJkLCBMYW5jYXN0ZXIsIFBBIDE3NjAxLCDQodCo0JA!5e0!3m2!1sru!2sru!4v1600706685552!5m2!1sru!2sru" width="100%" height="700" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
                 <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
                 <script>
-                    ymaps.ready(init);
+                    //ymaps.ready(init);
 
                     function init () {
                     
