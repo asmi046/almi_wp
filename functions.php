0ce0ee242b466c109d89b3249bf070f004a790eb
asmi046 <asmi046@gmail.com>
2020-09-29 00:08:44 +0300

    Замена картинок и Версия скриптов

--- functions.php
+++ functions.php
@@ -53,8 +53,8 @@ add_action( 'wp_enqueue_scripts', 'my_assets' );
 
 		// Подключение стилей 
 
-		$style_version = "1.0.5";
-		$scrypt_version = "1.0.5";
+		$style_version = "1.0.10";
+		$scrypt_version = "1.0.10";
 		
 		wp_enqueue_style("style-modal", get_template_directory_uri()."/css/jquery.arcticmodal-0.3.css", array(), $style_version, 'all'); //Модальные окна (стили)
 		wp_enqueue_style("style-lightbox", get_template_directory_uri()."/css/lightbox.min.css", array(), $style_version, 'all'); //Лайтбокс (стили)
